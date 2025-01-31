9a2f47ab93dd83a296544448745306e3b885b442
RED <140132638+GrPukaw@users.noreply.github.com>
2025-01-30 23:20:41 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-for (let i = 0; i < 1000; i++) {
-    const box = document.createElement('div');
-    box.className = 'box';
-    box.style.transform = `translate3d(${Math.random() * 2000 - 1000}px, ${Math.random() * 2000 - 1000}px, ${Math.random() * 2000 - 1000}px)`;
-    document.querySelector('.scene').appendChild(box);
-}
